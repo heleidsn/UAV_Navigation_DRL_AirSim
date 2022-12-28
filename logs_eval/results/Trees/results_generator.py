7792770350797f1d663ba17fca57df659c559c7e
He Lei <598269931@qq.com>
2022-12-28 09:47:48 +0800

    add eval results SimpleAvoid (or Maze）

--- logs_eval/results/Trees/results_generator.py
+++ logs_eval/results/Trees/results_generator.py
@@ -2,8 +2,12 @@ import numpy as np
 
 results = []
 
-results.append([294.9498467246312, 0.64, 0.34, 316.59375])
-results.append([336.4001846640506, 0.74, 0.24, 336.27027027027026])
-results.append([350.63767655345924, 0.84, 0.16, 340.07142857142856])
+results.append([16.434492206033905, 0.76, 0.24, 119.78947368421052])
+results.append([9.217483294028693, 0.7, 0.3, 123.08571428571429])
+results.append([9.431210764063954, 0.72, 0.28, 126.5])
 
-np.save('eval_50_NH_center_NH_center_SimpleMultirotor', np.array(results))
+results.append([24.151430446779333, 0.98, 0.02, 125.91836734693878])
+results.append([20.71481445587884, 1, 0, 131.98])
+results.append([17.80080238090519, 0.96, 0.04, 136.75])
+
+np.save('eval_50_SimpleAvoid_SimpleAvoid_SimpleMultirotor', np.array(results))
