dabf9275bfd2848c5e29445789003b4c51fb10aa
He Lei <598269931@qq.com>
2022-12-28 09:38:10 +0800

    update eval results NH_center

--- logs_eval/results/Trees/results_generator.py
+++ logs_eval/results/Trees/results_generator.py
@@ -2,12 +2,8 @@ import numpy as np
 
 results = []
 
-results.append([30.056801369453236, 0.98, 0.02, 167.55102040816325])
-results.append([35.29027739590203, 1, 0, 159.78])
-results.append([26.644441169876337, 1, 0, 176.02])
+results.append([294.9498467246312, 0.64, 0.34, 316.59375])
+results.append([336.4001846640506, 0.74, 0.24, 336.27027027027026])
+results.append([350.63767655345924, 0.84, 0.16, 340.07142857142856])
 
-results.append([29.622984700713218, 1, 0, 165.04])
-results.append([24.766614548715282, 0.98, 0.02, 167.42857142857142])
-results.append([27.566304489823043, 1, 0, 163.66])
-
-np.save('eval_50_Trees_Trees_SimpleMultirotor', np.array(results))
+np.save('eval_50_NH_center_NH_center_SimpleMultirotor', np.array(results))
